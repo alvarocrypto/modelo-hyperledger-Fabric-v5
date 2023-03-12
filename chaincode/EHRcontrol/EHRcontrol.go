0aac1c5dfd9af56ad831a234943a78fc4af4e3b3
alvarocrypto <adiaz@iacceso.cl>
2023-03-12 02:39:45 -0300

    se agrega nuevo chaincode

--- chaincode/EHRcontrol/EHRcontrol.go
+++ chaincode/EHRcontrol/EHRcontrol.go
@@ -8,7 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
