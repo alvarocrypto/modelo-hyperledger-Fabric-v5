b89b6a0a1409c81deb27bc8fbd15dca7e5fbd101
alvarocrypto <adiaz@iacceso.cl>
2023-03-12 23:22:49 -0300

    se agrega nuevo chaincode

--- chaincode/EHRcontrol/EHRcontrol.go
+++ chaincode/EHRcontrol/EHRcontrol.go
@@ -1,176 +1,88 @@
-/*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 package main
 
+/* Imports
+ * 4 utility libraries for formatting, handling bytes, reading and writing JSON, and string manipulation
+ * 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+ */
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 
-	"github.com/hyperledger/fabric-chaincode-go/shim"
-	pb "github.com/hyperledger/fabric-protos-go/peer"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	sc "github.com/hyperledger/fabric/protos/peer"
 )
 
-const ERROR_SYSTEM = "{\"code\":300, \"reason\": \"system error: %s\"}"
-const ERROR_WRONG_FORMAT = "{\"code\":301, \"reason\": \"command format is wrong\"}"
-const ERROR_ACCOUNT_EXISTING = "{\"code\":302, \"reason\": \"account already exists\"}"
-const ERROR_ACCOUNT_ABNORMAL = "{\"code\":303, \"reason\": \"abnormal account\"}"
-const ERROR_MONEY_NOT_ENOUGH = "{\"code\":304, \"reason\": \"account's money is not enough\"}"
-
-
-
-type SimpleChaincode struct {
-
+// Define the Smart Contract structure
+type SmartContract struct {
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	// nothing to do
-	return shim.Success(nil)
+// Define the car structure, with 4 properties.  Structure tags are used by encoding/json library
+type Rate struct {
+	Deal   string `json:"deal"`
+	Price  string `json:"price"`
 }
 
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	function, args := stub.GetFunctionAndParameters()
-
-	if function == "open" {
-		return t.Open(stub, args)
-	}
-	if function == "delete" {
-		return t.Delete(stub, args)
-	}
-	if function == "query" {
-		return t.Query(stub, args)
-	}
-	if function == "transfer" {
-		return t.Transfer(stub, args)
-	}
-
-	return shim.Error(ERROR_WRONG_FORMAT)
+/*
+ * The Init method is called when the Smart Contract "fabcar" is instantiated by the blockchain network
+ * Best practice is to have any Ledger initialization in separate function -- see initLedger()
+ */
+func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
+	return shim.Success(nil);
 }
 
-// open an account, should be [open account money]
-func (t *SimpleChaincode) Open(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 2 {
-		return shim.Error(ERROR_WRONG_FORMAT)
+/*
+ * The Invoke method is called as a result of an application request to run the Smart Contract "fabcar"
+ * The calling application program has also specified the particular smart contract function to be called, with arguments
+ */
+func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
+
+	// Retrieve the requested Smart Contract function and arguments
+	function, args := APIstub.GetFunctionAndParameters()
+	// Route to the appropriate handler function to interact with the ledger appropriately
+	if function == "queryRate" {
+		return s.queryRate(APIstub, args)
+	} else if function == "createRate" {
+		return s.createRate(APIstub, args)
 	}
 
-	account  := args[0]
-	money,err := stub.GetState(account)
-	if money != nil {
-		return shim.Error(ERROR_ACCOUNT_EXISTING)
-	}
-
-	_,err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error(ERROR_WRONG_FORMAT)
-	}
-
-	err = stub.PutState(account, []byte(args[1]))
-	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
-		return shim.Error(s)
-	}
-
-	return shim.Success(nil)
+	return shim.Error("Invalid Smart Contract function name.")
 }
 
-// delete an account, should be [delete account]
-func (t *SimpleChaincode) Delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (s *SmartContract) queryRate(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
+
 	if len(args) != 1 {
-		return shim.Error(ERROR_WRONG_FORMAT)
+		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	err := stub.DelState(args[0])
-	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
-		return shim.Error(s)
-	}
-
-	return shim.Success(nil)
+	carAsBytes, _ := APIstub.GetState(args[0])
+	return shim.Success(carAsBytes)
 }
 
-// query current money of the account,should be [query accout]
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error(ERROR_WRONG_FORMAT)
-	}
 
-	money, err := stub.GetState(args[0])
-	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
-		return shim.Error(s)
-	}
+func (s *SmartContract) createRate(APIstub shim.ChaincodeStubInterface, args []string) sc.Response {
 
-	if money == nil {
-		return shim.Error(ERROR_ACCOUNT_ABNORMAL)
-	}
-
-	return shim.Success(money)
-}
-
-// transfer money from account1 to account2, should be [transfer account1 account2 money]
-func (t *SimpleChaincode) Transfer(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 3 {
-		return shim.Error(ERROR_WRONG_FORMAT)
-	}
-	money, err := strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error(ERROR_WRONG_FORMAT)
+		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
 
-	moneyBytes1, err1 := stub.GetState(args[0])
-	if err1 != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err1.Error())
-		return shim.Error(s)
-	}
-	moneyBytes2, err2 := stub.GetState(args[1])
-	if err2 != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err2.Error())
-		return shim.Error(s)
-	}
-	if moneyBytes1 == nil || moneyBytes2 == nil {
-		return shim.Error(ERROR_ACCOUNT_ABNORMAL)
-	}
+	var rate = Rate{Deal: args[1], Price: args[2]}
 
-	money1, _ := strconv.Atoi(string(moneyBytes1))
-	money2, _ := strconv.Atoi(string(moneyBytes2))
-	if money1 < money {
-		return shim.Error(ERROR_MONEY_NOT_ENOUGH)
-	}
-
-	money1 -= money
-	money2 += money
-
-	err = stub.PutState(args[0], []byte(strconv.Itoa(money1)))
-	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
-		return shim.Error(s)
-	}
-
-	err = stub.PutState(args[1], []byte(strconv.Itoa(money2)))
-	if err != nil {
-		s := fmt.Sprintf(ERROR_SYSTEM, err.Error())
-		return shim.Error(s)
-	}
-
+	carAsBytes, _ := json.Marshal(rate)
+	APIstub.PutState(args[0], carAsBytes)
+	fmt.Println("new Rate has been created")
+	APIstub.SetEvent("rate_created", carAsBytes)
 	return shim.Success(nil)
 }
 
 
-func  main()  {
-	err := shim.Start(new(SimpleChaincode))
-	if err != nil {
-		fmt.Printf("Error starting chaincode: %v \n", err)
-	}
 
+
+// The main function is only relevant in unit test mode. Only included here for completeness.
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
 }
